f70659558beea650f82e59b96f4ec3ea89673108
K <celfrost@hotmail.com>
2014-07-07 15:43:11 -0400

    Added models for user with devise and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  validates :user_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :moto, presence: true
+  validates :bio, presence: true
+  validates :interests, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
